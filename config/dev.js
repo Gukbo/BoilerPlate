f9889ea2c283ee2f9d1a2e79881ca20b7e6247d5
Gukbo <2onesound@naver.com>
2023-04-03 15:40:42 +0900

    통신 보안

---
+++ config/dev.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://haneum:1313@hancluster.8r3alit.mongodb.net/?retryWrites=true&w=majority",
+};
--- index.js
+++ index.js
@@ -1,16 +1,36 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+const bodyParser = require("body-parser");
+const { user } = require("./models/User");
+const { User } = require("./models/User");
+
+const config = require("./config/key");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 const mongoose = require("mongoose");
 mongoose
-  .connect(
-    "mongodb+srv://haneum:1313@hancluster.8r3alit.mongodb.net/?retryWrites=true&w=majority",
-    {}
-  )
+  .connect(config.mongoURI, {})
   .then(() => console.log("MONGODB CONNECTED"))
   .catch((err) => console.log(err));
 
-app.get("/", (req, res) => res.send("Hello World !"));
+app.get("/", (req, res) => res.send("Hello World ! 기모링"));
+
+app.post("/register", async (req, res) => {
+  const user = new User(req.body);
+
+  const result = await user
+    .save()
+    .then(() => {
+      res.status(200).json({
+        success: true,
+      });
+    })
+    .catch((err) => {
+      res.json({ success: false, err });
+    });
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
